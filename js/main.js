5c582185f1a1e30cd17a6f45a1e8df496c4d4139
elishaolade <elishaolade@gmail.com>
2019-12-11 23:40:02 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// function iconMouseOver(){
+//   console.log(event.target)
+//   if(event.target.classList.contains('gh')){
+//     event.target.classList.toggle('icons-size-grow')
+//   }
+//   else if(event.target.classList.contains('fb')){
+//     event.target.classList.toggle('icons-size-grow')
+//   }
+//   else if(event.target.classList.contains('tw')){
+//     event.target.classList.toggle('icons-size-grow')
+//   }
+//   else if(event.target.classList.contains('inst')){
+//     event.target.classList.toggle('icons-size-grow')
+//   }
+// }
